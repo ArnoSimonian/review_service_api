2d83df719d544e1d88afcf1f40c245526fe8f610
arturrayanov <arturrayanov@yandex.ru>
2023-05-10 21:39:28 +0300

    Add description to Tttle

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -35,13 +35,16 @@ class Genre(models.Model):
 class Title(models.Model):
     name = models.CharField('название', max_length=50)
     year = models.DateField('год выпуска')
+    description = models.TextField('Описание',
+                                   max_length=250,
+                                   blank=True)
     category = models.ForeignKey(Category,
                                  on_delete=models.SET_NULL,
                                  related_name='titles',
                                  verbose_name='категория')
     genre = models.ManyToManyField(Genre,
                                    related_name='titles',
-                                   verbose_name='жанр'
+                                   verbose_name='жанр')
 
     class Meta:
         verbose_name = 'произведение'
