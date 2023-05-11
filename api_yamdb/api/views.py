a8a043ffa798723a35f0acc5ff5b92805f7219a2
arturrayanov <arturrayanov@yandex.ru>
2023-05-12 00:11:01 +0300

    Update models

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from reviews.models import Category, Genre, Title, GenreTitle, User, Review, Comment
 
-# Create your views here.
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Title
+        fields = '__all__'
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -11,33 +11,32 @@ CHOICES = (
 
 class Category(models.Model):
     name = models.CharField('название', max_length=256)
-    slug = models.SlugField('слаг', max_length=50)
+    slug = models.SlugField('слаг', max_length=50, unique=True)
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
 
     def __str__(self):
-        return self.name
+        return self.slug
 
 
 class Genre(models.Model):
     name = models.CharField('название', max_length=256)
-    slug = models.SlugField('слаг', max_length=50)
+    slug = models.SlugField('слаг', max_length=50, unique=True)
 
     class Meta:
         verbose_name = 'жанр'
         verbose_name_plural = 'жанры'
 
     def __str__(self):
-        return self.name
+        return self.slug
 
 
 class Title(models.Model):
     name = models.CharField('название', max_length=256)
-    year = models.DateField('год выпуска')
+    year = models.IntegerField('год выпуска')
     description = models.TextField('Описание',
-                                   max_length=250,
                                    blank=True)
     category = models.ForeignKey(Category,
                                  null=True,
@@ -45,8 +44,7 @@ class Title(models.Model):
                                  related_name='titles',
                                  verbose_name='категория')
     genre = models.ManyToManyField(Genre,
-                                   related_name='titles',
-                                   verbose_name='жанр')
+                                   through='GenreTitle')
 
     class Meta:
         verbose_name = 'произведение'
@@ -56,9 +54,27 @@ class Title(models.Model):
         return self.name
 
 
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.genre} {self.title}'
+
+
 class User(AbstractUser):
+    username = models.CharField('имя пользователя',
+                                max_length=150,
+                                unique=True)
+    email = models.CharField('почта', max_length=254, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
     bio = models.TextField('биография', blank=True)
-    role = models.CharField('роль', max_length=50, choices=CHOICES)
+    role = models.CharField('роль',
+                            max_length=50,
+                            choices=CHOICES,
+                            default='User',
+                            )
 
 
 class Review(models.Model):
